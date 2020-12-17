aacf94d17fe6102ea5f8f0101443cf15d8de4fb2
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2020-12-17 15:39:24 +0545

    First upload

---
+++ mytodo.js
@@ -0,0 +1,27 @@
+new Vue({
+    el:'#todo',
+    data:{
+        todos:[],
+        availabetodo:false
+    },
+    methods:{
+        addtodo:function(){
+            newtodo=document.getElementById("newtodo").value;
+            if(newtodo.length!=0)
+            {
+            this.todos.push(newtodo);
+            this.availabletodo=true;
+            newtodo='';
+            }
+            else{
+                window.alert('Add a Todo first');
+            }
+        }
+    }
+});
+function clearThis(target) {
+    target.value= "";
+}
+function done(newtarget){
+    newtarget.classList.toggle("done");
+}
